c16eeb5ff81f9a7ace093c4b5365987b14f0d066
frazermacrobert <frazermacrobert@gmail.com>
2025-09-28 23:27:16 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/leadership_heroes_demo/',
-  build: { outDir: 'docs' }
+  build: { outDir: 'docs' }   // <-- important
 })
